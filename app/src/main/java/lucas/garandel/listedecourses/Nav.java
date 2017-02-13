639172629d6af07e2ddb7ed2737030879243629e
root <root@linux.home>
2017-02-13 23:50:18 +0100

    Initialisation de la gestion par fragment - not working

--- app/src/main/java/lucas/garandel/listedecourses/Nav.java
+++ app/src/main/java/lucas/garandel/listedecourses/Nav.java
@@ -86,7 +86,7 @@ public class Nav extends AppCompatActivity
         } else if (id == R.id.nav_produits) {
             //startActivity(new Intent(Nav.this,Produits.class));
         } else if (id == R.id.nav_listes) {
-            //startActivity(new Intent(Nav.this,Listes.class));
+            //startActivity(new Intent(Nav.this,Listes.class)); test git
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
