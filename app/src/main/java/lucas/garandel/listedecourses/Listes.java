5e8ce1a036e7585f917fa1c49eb27beb1cecabcb
root <root@linux.home>
2017-02-13 22:30:26 +0100

    listes

--- app/src/main/java/lucas/garandel/listedecourses/Listes.java
+++ app/src/main/java/lucas/garandel/listedecourses/Listes.java
@@ -7,16 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Produits extends AppCompatActivity {
+public class Listes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_produits);
+        setContentView(R.layout.activity_listes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabProd);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
