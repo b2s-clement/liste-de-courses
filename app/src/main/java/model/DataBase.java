82ec99c1b5afb0f2c5f411e6ed5f2d90705cb1c8
b2s-clement <lucasc.ts1@gmail.com>
2017-02-15 11:56:16 +0100

    Implémentation de la base SQLite

--- app/src/main/java/model/DataBase.java
+++ app/src/main/java/model/DataBase.java
@@ -10,14 +10,48 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBase extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "base.sqlite";
+    private static final int VERSION = 1;
 
     public DataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context,DATABASE_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String request = "CREATE TABLE Magasins(" +
+                " _id integer primary key autoincrement," +
+                "name text not null," +
+                "ville text not null," +
+                "pays text not null);";
+        db.execSQL(request);
+        request = "CREATE TABLE Produits(" +
+                "_id integer primary key autoincrement," +
+                "categorie integer not null," +
+                "nom text not null," +
+                "code text not null);";
+        db.execSQL(request);
+        request = "CREATE TABLE Listes(" +
+                " _id integer," +
+                "id_produit integer," +
+                "id_magasin integer," +
+                "quantite float not null," +
+                "achete float not null," +
+                "FOREIGN KEY(id_produit) REFERENCES Produits(_id)," +
+                "FOREIGN KEY(id_magasin) REFERENCES Magasins(_id)," +
+                "PRIMARY KEY(_id,id_produit,id_magasin);";
+        db.execSQL(request);
+        request = "CREATE TABLE Vend(" +
+                "id_produit integer," +
+                "id_magasin integer," +
+                "prix float not null," +
+                "unité text not null," +
+                "rayon text," +
+                "promotion text," +
+                "FOREIGN KEY(id_produit) REFERENCES Produits(_id)," +
+                "FOREIGN KEY(id_magasin) REFERENCES Magasins(_id)," +
+                "PRIMARY KEY(id_produit,id_magasin); ";
+        db.execSQL(request);
     }
 
     @Override
